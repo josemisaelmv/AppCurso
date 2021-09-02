25b59b702be5f6b23198f23da16864d17a9da46a
josemisaelmv <josemisaelmv@gmail.com>
2021-09-02 05:56:22 -0700

    Create Functions on Cliente
    -Add inputs on view
    -Add input validations
    -Add functions to insert values
    
    1.0.0

--- AppCurso/Controllers/ClienteController.cs
+++ AppCurso/Controllers/ClienteController.cs
@@ -13,7 +13,7 @@ namespace AppCurso.Controllers
         public ActionResult Index()
         {
             List<ClienteCLS> listaCliente = null;
-            using (var bd=new BDPasajeEntities())
+            using (var bd = new BDPasajeEntities())
             {
                 listaCliente = (from cliente in bd.Cliente
                                 where cliente.BHABILITADO == 1
@@ -30,11 +30,55 @@ namespace AppCurso.Controllers
                                     telefonocelular = cliente.TELEFONOCELULAR
                                 }).ToList();
             }
-                return View(listaCliente);
+            return View(listaCliente);
+        }
+        List<SelectListItem> listaSexo;
+        private void llenarSexo()
+        {
+            using (var bd= new BDPasajeEntities())
+            {
+                listaSexo= (from sexo in bd.Sexo
+                           where sexo.BHABILITADO==1
+                           select new SelectListItem
+                           {
+                               Text = sexo.NOMBRE,
+                               Value = sexo.IIDSEXO.ToString()
+                           }).ToList();
+                listaSexo.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+            }
         }
         public ActionResult Agregar()
         {
+            llenarSexo();
+            ViewBag.lista = listaSexo;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(ClienteCLS oClienteCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                llenarSexo();
+                ViewBag.lista = listaSexo;
+                return ViewBag(oClienteCLS);
+            }
+            using (var bd = new BDPasajeEntities())
+            {
+                Cliente oCliente = new Cliente();
+                oCliente.NOMBRE = oClienteCLS.nombre;
+                oCliente.APPATERNO = oClienteCLS.appaterno;
+                oCliente.APMATERNO = oClienteCLS.apmaterno;
+                oCliente.EMAIL = oClienteCLS.email;
+                oCliente.DIRECCION = oClienteCLS.direccion;
+                oCliente.IIDSEXO = oClienteCLS.iidsexo;
+                oCliente.TELEFONOCELULAR = oClienteCLS.telefonocelular;
+                oCliente.TELEFONOFIJO = oClienteCLS.telefonofijo;
+                oCliente.BHABILITADO = 1;
+                bd.Cliente.Add(oCliente);
+                bd.SaveChanges();
+            }
+                return RedirectToAction("Index");
         }
     }
 }
--- AppCurso/Models/ClienteCLS.cs
+++ AppCurso/Models/ClienteCLS.cs
@@ -12,15 +12,20 @@ namespace AppCurso.Models
         public int iidcliente { get; set; }
         [Display(Name ="Nombre")]
         [Required]
+        [StringLength(100, ErrorMessage ="Longitud maxima 100")]
         public string nombre { get; set; }
         [Display(Name = "Apellido Paterno")]
         [Required]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
         public string appaterno { get; set; }
         [Display(Name = "Apellido Materno")]
         [Required]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
         public string apmaterno { get; set; }
         [Display(Name = "Email Cliente")]
         [Required]
+        [StringLength(200, ErrorMessage = "Longitud maxima 200")]
+        [EmailAddress(ErrorMessage = "Ingrese un email valido")]
         public string email { get; set; }
         [Display(Name ="Direccion")]
         [Required]
@@ -30,9 +35,11 @@ namespace AppCurso.Models
         public int iidsexo { get; set; }
         [Display(Name = "Telefono Fijo")]
         [Required]
+        [StringLength(10,ErrorMessage = "Longitud maxima 10")]
         public string telefonofijo { get; set; }
         [Display(Name = "Telefono Celular")]
         [Required]
+        [StringLength(10, ErrorMessage = "Longitud maxima 10")]
         public string telefonocelular { get; set; }
         public int bhabilitado { get; set; }
         public int btieneusuario { get; set; }
